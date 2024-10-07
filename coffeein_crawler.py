e8e24e212c123fab7ef33a428192f9958f075012
Adam Mitrenga <edemsikk@gmail.com>
2024-10-07 21:17:39 +0200

    refactor: convert function into class method for improved readability

--- coffeein_crawler.py
+++ coffeein_crawler.py
@@ -8,80 +8,103 @@ from bs4 import BeautifulSoup
 
 from metadata import Metadata
 
+class ScraperError(Exception):
+    """Custom exception for scraper errors."""
+    pass
 
-def find_metadata(output:str,filter = None)->bool:
-    page_exists = True
-    iterator = 1 
-    product_data = defaultdict(dict)
+class CoffeeinCrawler():
+    def __init__(self,base_url:str, ignore_words = None) -> None:
+        self.base_url = base_url
+        self.product_metadata = defaultdict(dict)
+        self.ignored_words = ignore_words
+
+    def get_items(self, match:str):
+        items_str = match.group(1)
+        items_str = items_str.replace("'", '"') 
+        items_str = re.sub(r',\s*}', '}', items_str)  # Remove trailing commas
+        items_str = re.sub(r',\s*]', ']', items_str)  # Remove trailing commas
+        return json.loads(f'[{items_str}]')  # Wrap in [] to make it a list
+  
+    def check_ignored_words(self, string)->bool:
+        if self.ignored_words:
+            for ignored_word in self.ignored_words:
+                if ignored_word.lower() in string.lower():
+                    return True
+        return False
     
-    while(page_exists):
+    def filter_items(self,items)->bool:
+        for item in items:
+            name_unfiltered = item.get('item_name')
+            decoded_name = name_unfiltered.encode('utf-8').decode('unicode_escape')
+            
+            link = unidecode.unidecode(name_unfiltered)
+            metadata = Metadata(
+                id=item.get('item_id'),
+                link=link,
+                name=decoded_name,
+                price=float(item.get('price')))                           
+
+            if metadata.id in self.product_metadata:
+                print("Found the same product twice. Ending...")
+                return False
+            
+            product_details = {
+                'name': metadata.name,
+                'price': metadata.price,
+                'link': metadata.link}
+            
+            if not self.check_ignored_words(metadata.name):
+                self.product_metadata[metadata.id] = product_details
+
+        print(f"Currently found {len(self.product_metadata)} products")
+        return True
+    
+    def filter_soup(self, soup: BeautifulSoup):
+        scripts = soup.find_all('script')
+        for script in scripts:
+            if 'view_item_list' not in script.text:
+                continue
+            # Use regex to extract the 'items' array
+            match = re.search(r'"items":\s*\[(.*?)\]\s*\}', script.text, re.DOTALL)
+            if not match:
+                # Raise custom error if the match is not found
+                raise ScraperError("Failed to find product data in script tags")
+            try:
+                items = self.get_items((match))
+                return self.filter_items(items)
+                
+            
+            except json.JSONDecodeError as e:
+                # Raise custom error if JSON decoding fails
+                raise ScraperError(f'JSON decode error: {e}') from e
         
-        url = f'https://www.coffeein.sk/kategoria/2/cerstvo-prazena-zrnkova-kava/{iterator}/'
-        iterator = iterator +1
-        response = requests.get(url)
+    def find_metadata(self, output:str)->bool:
+        page_exists = True
+        iterator = 1
+        
+        while(page_exists):
+            
+            url = f'{self.base_url}{iterator}/'
+            iterator = iterator +1
+            response = requests.get(url,timeout=60)
 
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, features="html.parser")
-            scripts = soup.find_all('script')
-                       
-            for script in scripts:
-                if 'view_item_list' in script.text:
-                    # Use regex to extract the 'items' array
-                    match = re.search(r'"items":\s*\[(.*?)\]\s*\}', script.text, re.DOTALL)
-                    if match:
-                        items_str = match.group(1)
-                        
-                        items_str = items_str.replace("'", '"') 
-                        items_str = re.sub(r',\s*}', '}', items_str)  # Remove trailing commas
-                        items_str = re.sub(r',\s*]', ']', items_str)  # Remove trailing commas
-                        try:
-                            items = json.loads(f'[{items_str}]')  # Wrap in [] to make it a list
-                            for item in items:
-                                name_unfiltered = item.get('item_name')
-                                decoded_name = name_unfiltered.encode('utf-8').decode('unicode_escape')
-                                #TODO : fix the link
-                                link = unidecode.unidecode(name_unfiltered)
-                                metadata = Metadata(
-                                    id=item.get('item_id'),
-                                    link=link,
-                                    name = decoded_name,
-                                    price=float(item.get('price'))
-                                )
-                                
-                                if metadata.id in product_data:
-                                    page_exists = False
-                                    print("Found the same product twice Ending")
-                                    break
-                                product_details = {
-                                    'name': metadata.name,
-                                    'price': metadata.price,
-                                    'link': metadata.link
-                                }
-                                if filter:
-                                    if filter.lower() in metadata.link.lower():
-                                        pass
-                                    else:
-                                        product_data[metadata.id] = product_details 
-                                else:
-                                    product_data[metadata.id] = product_details
-                            print(f"Currently found {len(product_data)}")
-                        except json.JSONDecodeError as e:
-                            print(f"JSON decode error: {e}")
-                    break
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, features="html.parser")
+                if not self.filter_soup(soup):
+                    page_exists = False    
+
             else:
-                print("Product data script not found.")
+                print(f"Failed to retrieve the page. Status code: {response.status_code}")
+                page_exists = False
 
-        else:
-            print(f"Failed to retrieve the page. Status code: {response.status_code}")
-            page_exists = False
-
-    with open(output, 'w') as json_file:
-        json.dump(product_data, json_file, indent=4)
-    return True
+        with open(output, 'w') as json_file:
+            json.dump(self.product_metadata, json_file, indent=4)
+        return True
 
 def main():
-    find_metadata('coffein_metadata.json',filter="tasting pack")
-
-        
+    cc = CoffeeinCrawler(base_url ='https://www.coffeein.sk/kategoria/2/cerstvo-prazena-zrnkova-kava/',ignore_words=["tasting pack"])
+    cc.find_metadata('coffein_metadata.json')
+      
 if __name__ == "__main__":
     main()
+    
