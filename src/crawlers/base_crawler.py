94c812c75a9d857c1dc3cbff748d0354cc1c936f
Adam Mitrenga <edemsikk@gmail.com>
2025-03-05 14:24:23 +0100

    🦄 refactor: Union output

--- src/crawlers/base_crawler.py
+++ src/crawlers/base_crawler.py
@@ -1,13 +1,14 @@
 from abc import ABC, abstractmethod
 from typing import Union
 
+
 class Crawler(ABC):
     @abstractmethod
-    def find_metadata(self, metadata_url_base: str) -> Union[dict,None]:
+    def find_metadata(self, metadata_url_base: str) -> Union[dict, None]:
         """finds metadata for all coffe products"""
         pass
 
     @abstractmethod
-    def find_coffee(self, coffe_url_base: str) -> Union[dict,None]:
+    def find_coffee(self, coffe_url_base: str) -> Union[dict, None]:
         """finds specific coffe and all information about it"""
         pass
