2b2699989aabd7f32cfd56fc4555f5517258e129
Adam Mitrenga <edemsikk@gmail.com>
2025-03-09 00:36:32 +0100

    ✨ feat: COFFEEIN,SOUP,META

--- src/assets/constants.py
+++ src/assets/constants.py
@@ -1,1 +1,40 @@
+# CRAWLER
 COFFEIN_MAIN_COFFE_PAGE = "kategoria/2/cerstvo-prazena-zrnkova-kava/"
+
+## DATABASE 
+TABLE_METADATA = "metadata"
+TABLE_COFFEE = "coffee"
+
+
+# COLUMNS METADATA
+ID = "id"
+PAGE_ID = "page_id"
+NAME = "name"
+ORIGIN = "origin"
+CHANGED_AT = "changed_at"
+PRICE = "price"
+DETAIL_LINK = "detail_link"
+IMAGE_LINK = "image_link"
+
+
+# BEAUTIFUL SOUP 
+DIV = 'div'
+SPAN = 'span'
+SCRIPT = 'script'
+
+H1 = 'h1'
+
+# COFFEEIN EXTRAS
+ARABICA_COFFEEIN = "100 % Arabica"
+ROBUSTA_COFFEEIN = "100 % Robusta"
+DECAF = "bezkofeinová"
+DECAF_MISSTYPE = "bezkofeínová"
+PRICE = "price"
+ITEM_NAME = "item_name"
+UNKNONW = "Unknown"
+DESCRIPTION = 'description'
+COFFEE_ORIGIN = 'coffee_origin'
+SWEETNEST_COFFEEIN = "sladkosť"
+ACIDITY_COFFEEIN = "acidita"
+BITTERNESS_COFFEEIN = "horkosť"
+BODY_COFFEIN = "telo"
