89387fd34fd32cb4e108c28623c42535e2224452
Adam Mitrenga <edemsikk@gmail.com>
2025-03-09 00:37:49 +0100

    ✨ feat: used assets

--- src/database/supabase_db.py
+++ src/database/supabase_db.py
@@ -1,9 +1,10 @@
 from supabase import create_client, Client
 import os
-from typing import Dict, List, Tuple
+from typing import Dict, List
+from assets.constants import TABLE_METADATA,NAME,PAGE_ID,PRICE,DETAIL_LINK,ORIGIN,ID
 from database.db_interface import Database
 from models.metadata import Metadata
-from models.coffee import Coffee, Origin, Taste, PackageInformation, Review
+from models.coffee import Coffee, Origin, Taste, Review
 
 
 class SupabaseDB(Database):
@@ -13,24 +14,21 @@ class SupabaseDB(Database):
         self.supabase: Client = create_client(url, key)
 
     def delete_metadata(self, id: str) -> None:
-        self.supabase.table("metadata").delete().eq("id", id).execute()
+        self.supabase.table(TABLE_METADATA).delete().eq(ID, id).execute()
 
 
 
     def delete_old_metadata(self, new_metadata_list: List[Metadata]) -> List[int]:
-        # Get all existing ids
-        existing_ids = self.supabase.table("metadata").select("page_id").execute().data
-        existing_id_set = set(item['page_id'] for item in existing_ids)
+        existing_ids = self.supabase.table(TABLE_METADATA).select(PAGE_ID).execute().data
+        existing_id_set = set(item[PAGE_ID] for item in existing_ids)
         
-        # Find ids to delete
         new_id_set = set(metadata.page_id for metadata in new_metadata_list)
         ids_to_delete = existing_id_set - new_id_set
 
         deleted_ids = []
         if ids_to_delete:
-            # Delete the metadata with the ids to delete
             for id in ids_to_delete:
-                self.supabase.table("metadata").delete().eq("page_id", id).execute()
+                self.supabase.table(TABLE_METADATA).delete().eq(PAGE_ID, id).execute()
                 deleted_ids.append(id)
                 
         return deleted_ids
@@ -39,23 +37,21 @@ class SupabaseDB(Database):
         created = []
         updated = []
         for metadata in new_metadata_list:
-            # Prepare the data for upsert, excluding image_link
             upsert_data = {
-                "page_id": metadata.page_id,
-                "origin": metadata.origin,
-                "name": metadata.name,
-                "price": metadata.price,
-                "detail_link": metadata.detail_link,
+                PAGE_ID: metadata.page_id,
+                ORIGIN: metadata.origin,
+                NAME: metadata.name,
+                PRICE: metadata.price,
+                DETAIL_LINK: metadata.detail_link,
             }
             
-            # Check if the metadata already exists
-            existing = self.supabase.table("metadata").select("*").eq("id", metadata.page_id).execute().data
+            existing = self.supabase.table(TABLE_METADATA).select("*").eq(ID, metadata.page_id).execute().data
             
             if existing:
-                result = self.supabase.table("metadata").update(upsert_data).eq("id", metadata.page_id).execute()
+                result = self.supabase.table(TABLE_METADATA).update(upsert_data).eq(ID, metadata.page_id).execute()
                 updated.extend(result.data)
             else:
-                result = self.supabase.table("metadata").insert(upsert_data).execute()
+                result = self.supabase.table(TABLE_METADATA).insert(upsert_data).execute()
                 created.extend(result.data)
 
-        return {'created':created,'updated':updated}
+        return {'created': created,'updated': updated}
