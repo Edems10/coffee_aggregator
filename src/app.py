ffb405ceb7cd5d0f03852b9b5099cc44afdd5b80
Adam Mitrenga <edemsikk@gmail.com>
2025-03-09 00:36:10 +0100

    🦄 refactor: testing

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
 from factory.crawler_factory import CrawlerFactory
 from models.page import PageType
-from assets.constants import COFFEIN_MAIN_COFFE_PAGE
+from assets.constants import COFFEIN_MAIN_COFFE_PAGE,DETAIL_LINK
 from factory.processor_factory import ProcessorFactory
 from database.supabase_db import SupabaseDB
+import pickle
 
 def main():
 
@@ -10,14 +11,23 @@ def main():
     processor = ProcessorFactory.create_processor(PageType.COFFEEIN, ignored_coffes = ["tasting pack"])
     supabase = SupabaseDB()
     metadata_set = set()
-    for metadata_soup in crawler.find_metadata(COFFEIN_MAIN_COFFE_PAGE):
-        metadata_batch = processor.process_metadata(metadata_soup)
-        metadata_set.update(metadata_batch)
+    # for metadata_soup in crawler.find_metadata(COFFEIN_MAIN_COFFE_PAGE):
+    #     metadata_batch = processor.process_metadata(metadata_soup)
+    #     metadata_set.update(metadata_batch)
     
     
-    supabase.update_metadata(list(metadata_set))
-    supabase.delete_old_metadata(list(metadata_set))
+    # updated_dict = supabase.update_metadata(list(metadata_set))
+    # # This should be migrate to old also tracking price would be nice
+    # supabase.delete_old_metadata(list(metadata_set))
     
+    # TESTING
+    with open('metadata_set.pkl', 'rb') as f:
+            metadata_set = pickle.load(f)
+            print("Loaded metadata from file.")
+    
+    for coffee_soup in crawler.find_coffee(list(metadata_set)):
+        coffee = processor.process_coffee(coffee_soup)
+            
     
     
 if __name__ == "__main__":
